3799d4bf0a042dc9d8966bd1e8ad641668a1cbe4
Manami Ichikawa <mana@Ms-MacBook-Pro.local>
2017-03-12 14:50:53 +0900

    03/06 課題10

---
+++ AlertSheet/AlertSeet/SheetViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlertSeet
+//
+//  Created by Manami Ichikawa on 3/12/17.
+//  Copyright © 2017 Manami Ichikawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SheetViewController : UIViewController
+
+
+@end
+
---
+++ AlertSheet/AlertSeet/SheetViewController.m
@@ -0,0 +1,62 @@
+//
+//  ViewController.m
+//  AlertSeet
+//
+//  Created by Manami Ichikawa on 3/12/17.
+//  Copyright © 2017 Manami Ichikawa. All rights reserved.
+//
+
+#import "SheetViewController.h"
+
+@interface SheetViewController ()
+
+@end
+
+@implementation SheetViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)pushBtn:(id)sender {
+    
+        
+        //タイトルとメッセージ表示
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"タイトル"
+                                                                       message:@"action sheet."
+                                                                preferredStyle:UIAlertControllerStyleActionSheet];
+        
+        //選択肢を表示
+        UIAlertAction *firstAction = [UIAlertAction actionWithTitle:@"Twitter"
+                                                              style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+                                                                  NSLog(@"You pressed Twitter");
+                                                              }];
+        
+        UIAlertAction *secondAction = [UIAlertAction actionWithTitle:@"Facebook"
+                                                               style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+                                                                   NSLog(@"You pressed Facebook");
+                                                               }];
+        
+        UIAlertAction *thirdAction = [UIAlertAction actionWithTitle:@"Line"
+                                                              style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+                                                                  NSLog(@"You pressed Line");
+                                                              }];
+        
+        [alert addAction:firstAction];
+        [alert addAction:secondAction];
+        [alert addAction:thirdAction];
+        
+        
+        [self presentViewController:alert animated:YES completion:nil];
+    
+
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
