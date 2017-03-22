6ae1287673b3594b4fd2337c6594e8dcfa46743a
Manami Ichikawa <mana@Ms-MacBook-Pro.local>
2017-03-23 01:47:13 +0900

    03/09 課題2-1-15完了しました

---
+++ UITabBarController/UITabBarController/SecondViewController.h
@@ -0,0 +1,14 @@
+//
+//  SecondViewController.h
+//  UITabBarController
+//
+//  Created by Manami Ichikawa on 3/9/17.
+//  Copyright © 2017 Manami Ichikawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *label2;
+
+@end
